f37ac8860d93fd99ab85c66a05db3360f2aa46d3
Francine Souza <francine.ssouza@outlook.com>
2021-12-18 05:06:14 -0200

    caixa de pesquisa

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ function App() {
   return (
     <div className='App'>
      <h1>Cosméticos-Plus</h1>
+  
+
+
+      <input type='text' placeholder= 'Buscar'>
+</input>
     </div>
-  );
+
+    
+    );
+
+
 }
 
 export default App;
